5906f65af9b4ab5fdcff81b3359d30b5bcb9df3d
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-29 07:09:01 +0200

    End

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			randomMail: [],
+		}
+	},
+	methods: {
+		getRandomMail() {
+
+            this.randomMail = [];
+
+
+            for (let i = 0; i < 10; i++) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(serverResponse => this.randomMail.push(serverResponse.data.response));
+            } 
+		},
+
+
+
+
+        
+	},
+	created() {
+	    this.getRandomMail();
+	},
+});
+
+app.mount('#app');
